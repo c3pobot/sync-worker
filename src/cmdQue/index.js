56109230de9b89d0f923cf6ca8025c58b179a791
scuba <scuba75@gmail.com>
2025-01-30 09:12:45 -0500

    update

--- src/cmdQue/index.js
+++ src/cmdQue/index.js
@@ -16,7 +16,7 @@ const processCmd = async(obj = {})=>{
 }
 const start = async()=>{
   if(consumer) await consumer.close()
-  consumer = rabbitmq.createConsumer({ consumerTag: POD_NAME, concurrency: 1, qos: { prefetchCount: 1 }, queue: QUE_NAME, queueOptions: { durable: true, arguments: { 'x-queue-type': 'quorum' } } }, processCmd)
+  consumer = rabbitmq.createConsumer({ consumerTag: POD_NAME, concurrency: 1, qos: { prefetchCount: 1 }, queue: QUE_NAME }, processCmd)
   consumer.on('error', (err)=>{
     log.info(err)
   })
